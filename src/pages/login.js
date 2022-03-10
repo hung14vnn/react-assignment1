5cfde7e6e81c9491fe52665f537f0c30234f35fd
hung14vnn <74287834+hung14vnn@users.noreply.github.com>
2022-03-10 08:53:50 +0700

    Update login.js

--- src/pages/login.js
+++ src/pages/login.js
@@ -4,6 +4,10 @@ const LoginPage = () => {
         username: '',
         password: '',
     });
+    const [touched, setTouched] = React.useState({
+        username: false,
+        password: false
+    });
     const handleChange = (event) => {
         setValues({
             ...value,
@@ -11,27 +15,62 @@ const LoginPage = () => {
         });
         };  
 
-    // const handleSubmit = (event) => {
-    //     event.preventDefault();
-    // };
+    const handleSubmit = (event) => {
+        event.preventDefault();
+    };
+    const handleInputBlur = (event) => {
+        setTouched({
+            ...touched,
+            [event.target.name]: true
+        });
+    };
+    const validateUsername = (username) => {
+        if(!username) return 'Username is required';
+        if(username.length < 3) return 'Username must be at least 3 characters long';
+        const validUsername = String(username)
+        .toLowerCase()
+        .match(/^[a-z0-9]+$/);
+        if(!validUsername) return 'Username can only contain letters and numbers';
+        return '';
+    };
+    
+    const validatePassword = (password) => {
+        if(!password) return 'Password is required';
+        if(password.length < 8) return 'Password must be at least 8 characters';
+        if(password.length > 32) return 'Password must be less than 32 characters';
+        return '';
+    };
+    const errorMessage = {
+        username: validateUsername(value.username),
+        password: validatePassword(value.password)
+    };
+    const validForm = errorMessage.password || errorMessage.username;
         return (
         <div>
-        <form>
+        <form onSubmit={handleSubmit}>
+           <div>
             <input 
             style={{display: 'block', margin:"20px"}} 
             type="text" 
             value={value.username}
             onChange={handleChange}
+            onBlur={handleInputBlur}
             name = "username"
             placeholder="Username" />
+            {touched.username && <p style={{color: 'red', margin:"30px"}}>{errorMessage.username}</p>}
+            </div>
+            <div>
             <input 
             style={{display: 'block', margin:"20px"}} 
             type="password" 
             value={value.password}
             onChange={handleChange}
+            onBlur={handleInputBlur}
             name = "password"
             placeholder="Password" />
-            <button style={{display: 'block', margin:"20px"}} type="submit">Login</button>
+            {touched.password && <p style={{color: 'red',margin:"30px"}}>{errorMessage.password}</p>}
+            </div>
+            <button disabled={validForm} style={{display: 'block', margin:"20px"}} type="submit">Login</button>
         </form>
         </div>
     );
