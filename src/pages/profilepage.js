a985026e47786035f088291be71a8b9030fdfaa9
hung14vnn <valkyriea10@gmail.com>
2022-03-14 01:15:00 +0700

    1 file modified

--- src/pages/profilepage.js
+++ src/pages/profilepage.js
@@ -1,33 +1,35 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useState, useEffect } from 'react';
-import LoginPage from "./login"; 
+import LoginPage from './login';
 export default function ProfilePage() {
-    const [user,setUser]= useState();
-    const token = localStorage.getItem('token');
-    const userId = localStorage.getItem('userId');
+  const [user, setUser] = useState();
+  const token = localStorage.getItem('token');
+  const userId = localStorage.getItem('userId');
 
-    useEffect(() => {
-        fetch('https://60dff0ba6b689e001788c858.mockapi.io/users/'+userId, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer '+token
-            }
-        })
-        .then(response => response.json())
-        .then(json => setUser(json));
-    }, [token, userId]);
-    return user !== undefined ? (
-        <div>
-            <ul>
-                <div>ID: {user?.id}</div>
-                <div>Name: {user?.name}</div>
-            </ul>
-            <Link to="/">Go Home</Link>
-        </div>
-    ):(
-        <><p>Log in is required</p><LoginPage /></>
-    );
+  useEffect(() => {
+    fetch('https://60dff0ba6b689e001788c858.mockapi.io/users/' + userId, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + token,
+      },
+    })
+      .then((response) => response.json())
+      .then((json) => setUser(json));
+  }, [token, userId]);
+  return user !== '' ? (
+    <div>
+      <ul>
+        <div>ID: {user?.id}</div>
+        <div>Name: {user?.name}</div>
+      </ul>
+      <Link to="/">Go Home</Link>
+    </div>
+  ) : (
+    <>
+      <p>Login is required</p>
+      <LoginPage />
+    </>
+  );
 }
-     
