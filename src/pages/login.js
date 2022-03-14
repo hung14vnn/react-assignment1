b5436b979d2c0785a91b18b2f592bfa3ae10c165
hung14vnn <valkyriea10@gmail.com>
2022-03-14 23:45:08 +0700

    2 files modified

--- src/pages/login.js
+++ src/pages/login.js
@@ -5,108 +5,141 @@ import FormHelperText from '@mui/material/FormHelperText';
 import Checkbox from '@mui/material/Checkbox';
 import FormControlLabel from '@mui/material/FormControlLabel';
 const LoginPage = () => {
-    //     const rmCheck = document.getElementById("rememberMe"),
-//     usertoken = "3";
+  //     const rmCheck = document.getElementById("rememberMe"),
+  //     usertoken = "3";
 
-// if (localStorage.checkbox && localStorage.checkbox !== "") {
-//   rmCheck.setAttribute("checked", "checked");
-//   localStorage.setItem('userId', usertoken);
-// } else {
-//   rmCheck.removeAttribute("checked");
-//   localStorage.setItem('userId', undefined);
-// }
-    const [value, setValues] = React.useState({
-        username: '',
-        password: '',
+  // if (localStorage.checkbox && localStorage.checkbox !== "") {
+  //   rmCheck.setAttribute("checked", "checked");
+  //   localStorage.setItem('userId', usertoken);
+  // } else {
+  //   rmCheck.removeAttribute("checked");
+  //   localStorage.setItem('userId', undefined);
+  // }
+  const [value, setValues] = React.useState({
+    username: '',
+    password: '',
+  });
+  const [touched, setTouched] = React.useState({
+    username: false,
+    password: false,
+  });
+  const handleChange = (event) => {
+    setValues({
+      ...value,
+      [event.target.name]: event.target.value,
     });
-    const [touched, setTouched] = React.useState({
-        username: false,
-        password: false
-    });
-    const handleChange = (event) => {
-        setValues({
-            ...value,
-            [event.target.name]: event.target.value
-        });
-        };  
+  };
 
-    const handleSubmit = (event) => {
-        fetch('https://60dff0ba6b689e001788c858.mockapi.io/tokens', {
-            method: 'GET',
+  const handleSubmit = (event) => {
+    fetch('https://60dff0ba6b689e001788c858.mockapi.io/tokens', {
+      method: 'GET',
     })
-    .then(function (response) {
+      .then(function (response) {
         return response.json();
-    })
-    .then(function (json) {
-        const {token, userId} = json;
+      })
+      .then(function (json) {
+        // if (rmCheck.checked && emailInput.value !== '') {
+        const { token, userId } = json;
         localStorage.setItem('token', token);
         localStorage.setItem('userId', userId);
         window.location.href = '/profile';
+        // } else {
+        //   const { token, userId } = json;
+        //   localStorage.setItem('token', undefied);
+        //   localStorage.setItem('userId', undefied);
+        // }
+      });
+  };
+  const handleInputBlur = (event) => {
+    setTouched({
+      ...touched,
+      [event.target.name]: true,
     });
-    };
-    const handleInputBlur = (event) => {
-        setTouched({
-            ...touched,
-            [event.target.name]: true
-        });
-    };
-    const validateUsername = (username) => {
-        if(!username) return 'Username is required';
-        if(username.length < 3) return 'Username must be at least 3 characters long';
-        const validUsername = String(username)
-        .toLowerCase()
-        .match(/^[a-z0-9]+$/);
-        if(!validUsername) return 'Username can only contain letters and numbers';
-        return '';
-    };
-    
-    const validatePassword = (password) => {
-        if(!password) return 'Password is required';
-        if(password.length < 8) return 'Password must be at least 8 characters';
-        if(password.length > 32) return 'Password must be less than 32 characters';
-        return '';
-    };
-    const errorMessage = {
-        username: validateUsername(value.username),
-        password: validatePassword(value.password)
-    };
-    const validForm = errorMessage.password || errorMessage.username;
-        return (
+  };
+  const validateUsername = (username) => {
+    if (!username) return 'Username is required';
+    if (username.length < 3)
+      return 'Username must be at least 3 characters long';
+    const validUsername = String(username)
+      .toLowerCase()
+      .match(/^[a-z0-9]+$/);
+    if (!validUsername) return 'Username can only contain letters and numbers';
+    return '';
+  };
+
+  const validatePassword = (password) => {
+    if (!password) return 'Password is required';
+    if (password.length < 8) return 'Password must be at least 8 characters';
+    if (password.length > 32) return 'Password must be less than 32 characters';
+    return '';
+  };
+  const errorMessage = {
+    username: validateUsername(value.username),
+    password: validatePassword(value.password),
+  };
+  const validForm = errorMessage.password || errorMessage.username;
+  return (
+    <div>
+      <form>
         <div>
-        <form>
-           <div>
-            <TextField
-            style={{display: 'block', margin:"20px", size:"small"}} 
-            type="text" 
+          <TextField
+            style={{ display: 'block', margin: '20px', size: 'small' }}
+            type="text"
             value={value.username}
             onChange={handleChange}
             onBlur={handleInputBlur}
-            name = "username"
+            name="username"
             placeholder="Username"
             label="Username"
-            variant="outlined"  />
-            {touched.username && <FormHelperText style={{display: 'block', margin:"20px", color:"red"}}>{errorMessage.username}</FormHelperText>}
-            </div>
-            <div>
-            <TextField
-            style={{display: 'block', margin:"20px", size : "small"}} 
-            type="password" 
+            variant="outlined"
+          />
+          {touched.username && (
+            <FormHelperText
+              style={{ display: 'block', margin: '20px', color: 'red' }}
+            >
+              {errorMessage.username}
+            </FormHelperText>
+          )}
+        </div>
+        <div>
+          <TextField
+            style={{ display: 'block', margin: '20px', size: 'small' }}
+            type="password"
             value={value.password}
             onChange={handleChange}
             onBlur={handleInputBlur}
-            name = "password"
+            name="password"
             placeholder="Password"
             label="Password"
-            variant="outlined" />
-            {touched.password && <FormHelperText style={{display: 'block', margin:"20px", color:"red"}}>{errorMessage.password}</FormHelperText>}
-            </div>
-            <FormControlLabel style={{display: 'block', margin:"20px", size : "small"}} value="lsRememberMe" id="rememberMe" control={<Checkbox defaultChecked />} label="Remember me" />
-            
-            <Button variant="contained" onClick={handleSubmit} disabled={validForm} style={{display: 'block', margin:"20px"}} type="submit">Login</Button>
-        </form>
+            variant="outlined"
+          />
+          {touched.password && (
+            <FormHelperText
+              style={{ display: 'block', margin: '20px', color: 'red' }}
+            >
+              {errorMessage.password}
+            </FormHelperText>
+          )}
         </div>
-    );
+        <FormControlLabel
+          style={{ display: 'block', margin: '20px', size: 'small' }}
+          value="lsRememberMe"
+          id="rememberMe"
+          control={<Checkbox defaultChecked />}
+          label="Remember me"
+        />
 
-
+        <Button
+          variant="contained"
+          onClick={handleSubmit}
+          disabled={validForm}
+          style={{ display: 'block', margin: '20px' }}
+          type="submit"
+        >
+          Login
+        </Button>
+      </form>
+    </div>
+  );
 };
 export default LoginPage;
--- src/pages/postpage.js
+++ src/pages/postpage.js
@@ -10,42 +10,53 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import Button from '@mui/material/Button';
 export default function PostPage() {
-    const [data, setData] = useState();
-    useEffect(()=>{
-        fetch('https://jsonplaceholder.typicode.com/posts')
-        .then(response => response.json())
-        .then(json => setData(json));
-    },100)
-    return (
-    
-     <TableContainer component={Paper}>
+  const [data, setData] = useState();
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then((response) => response.json())
+      .then((json) => setData(json));
+  }, []);
+  return (
+    <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
             <TableCell>ID</TableCell>
-            <TableCell >Title</TableCell>
-            <TableCell >Action</TableCell>
+            <TableCell>Title</TableCell>
+            <TableCell>Action</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-        {data && data.map(post => (
-            <TableRow
-            key={post.id}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-              {post.id}
-              </TableCell>
-              <TableCell >{post.title}</TableCell>
-              <TableCell ><Button variant="outlined" color="success"><Link to={`/posts/${post.id}`}>View</Link></Button></TableCell>
-              <TableCell ><Button variant="outlined" color="error" onClick={()=>{setData(data.filter(item => item.id !== post.id))}}>Delete</Button></TableCell>
-            </TableRow>
-          ))}
+          {data &&
+            data.map((post) => (
+              <TableRow
+                key={post.id}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {post.id}
+                </TableCell>
+                <TableCell>{post.title}</TableCell>
+                <TableCell>
+                  <Button variant="outlined" color="success">
+                    <Link to={`/posts/${post.id}`}>View</Link>
+                  </Button>
+                </TableCell>
+                <TableCell>
+                  <Button
+                    variant="outlined"
+                    color="error"
+                    onClick={() => {
+                      setData(data.filter((item) => item.id !== post.id));
+                    }}
+                  >
+                    Delete
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))}
         </TableBody>
       </Table>
     </TableContainer>
-    
-    );
-        
+  );
 }
-     
